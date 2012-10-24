b6bc1f6e320c05ea6b63dd5d0af687ec475d77c2
Trevor Norris <trev.norris@gmail.com>
2012-10-24 16:07:45 -0700

    Small syntax update and logic reorder

--- src/jqml.js
+++ src/jqml.js
@@ -4,16 +4,15 @@
  * MIT License: http://www.opensource.org/licenses/mit-license.php */
 (function(document, global) {
 
-var toString = Object.prototype.toString,
+var toString = {}.toString,
 
 	// set element attributes
 	// check for IE specific functionality and set function accordingly
 	setEleAttr = document.documentElement.style.setAttribute ? function(args, iTem) {
-		var aKey;
 		// loop through and apply attributes
-		for (aKey in args) {
+		for (var aKey in args) {
 			// check if aKey is 'style' attribute
-			if (aKey != 'style') {
+			if (aKey !== 'style') {
 				iTem.setAttribute(aKey, args[aKey]);
 			} else {
 				// technique from http://www.kadimi.com/en/setattribute-getattribute-315
@@ -22,9 +21,8 @@ var toString = Object.prototype.toString,
 		}
 		return iTem;
 	} : function(args, iTem) {
-		var aKey;
 		// loop through and apply attributes
-		for (aKey in args) {
+		for (var aKey in args) {
 				iTem.setAttribute(aKey, args[aKey]);
 		}
 		return iTem;
@@ -44,14 +42,11 @@ var toString = Object.prototype.toString,
 function jqml(elems) {
 	// create/set element
 	var node = elems[0].nodeType ? elems[0] : document.createElement(elems[0]),
-		i, j;
+		i = 1, j;
 	// loop though passed arguments
-	for (i = 1; i < elems.length; i++) {
-		// check if string or number
-		if (isStringy(elems[i])) {
-			node.appendChild(document.createTextNode(elems[i]));
+	for (; i < elems.length; i++) {
 		// check if argument is array
-		} else if (isArray(elems[i])) {
+		if (isArray(elems[i])) {
 			if (isArray(elems[i][0])) {
 				for (j = 0; j < elems[i].length; j++) {
 					node.appendChild(jqml(elems[i][j]));
@@ -59,6 +54,9 @@ function jqml(elems) {
 			} else {
 				node.appendChild(jqml(elems[i]));
 			}
+		// check if string or number
+		} else if (isStringy(elems[i])) {
+			node.appendChild(document.createTextNode(elems[i]));
 		// check if DOM element
 		} else if (elems[i].nodeType) {
 			node.appendChild(elems[i]);
